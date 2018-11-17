9ba9cc7a2c1c47cd36652cd6f2315e8e1a4ea290
suhantudor <suhantudor@gmail.com>
2018-11-18 01:00:50 +0200

    add hot reload to configStore.js

--- snippets/src/store/configStore.js
+++ snippets/src/store/configStore.js
@@ -1,18 +1,22 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import monitorReducersEnhancer from '../enhancers/monitorReducers'
-// import loggerMiddleware from '../middleware/logger'
 import fetchReducer from '../reducers/reducers'
 
 const loggerMiddleware = createLogger()
 
-export default function configureStore(preloadedState) {
-  const middlewares = [loggerMiddleware, thunkMiddleware]
-  const middlewareEnhancer = applyMiddleware(...middlewares)
-  const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
-  const composedEnhancers = compose(...enhancers)
-  const store = createStore(fetchReducer, preloadedState, composedEnhancers)
-  return store
+const configureStore = (preloadedState) => {
+    const middlewares = [loggerMiddleware, thunkMiddleware]
+    const middlewareEnhancer = applyMiddleware(...middlewares)
+    const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
+    const composedEnhancers = composeWithDevTools(compose(...enhancers))
+    const store = createStore(fetchReducer, preloadedState, composedEnhancers)
+    if (process.env.NODE_ENV !== 'production' && module.hot) {
+        module.hot.accept('../reducers/reducers', () => store.replaceReducer(fetchReducer))
+    }
+    return store
 }
+export default configureStore
