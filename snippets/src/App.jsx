8fc84cbdb6e53184da6ebd6d8d4dc4bf24f589d6
tudorsuhan <suhantudor@gmail.com>
2019-01-16 11:15:23 +0200

    fix errors

--- snippets/src/App.jsx
+++ snippets/src/App.jsx
@@ -1,33 +1,20 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import configureStore from './store/configureStore'
-import './styles/app.css'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+import './styles/app.css';
 
-import Routes from './rooter/Router'
+import Routes from './rooter/Router';
 
-const store = configureStore()
+const store = configureStore();
+console.log('Redux store state', store.getState()); // GET default state
 
-<<<<<<< HEAD
-console.log('Redux store state', store.getState()) // GET default state
-=======
-console.log(store.getState()) // GET default state
->>>>>>> a49ebf4fca167770181ea2f80b27ae3780e4b1a9
+const App = () => (
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>
+);
 
-export default class App extends Component {
-
-    static propTypes = {
-        history: PropTypes.object
-    }
-
-    render() {
-        return(
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Routes />
-                </BrowserRouter>
-            </Provider>
-        );
-    }
-}
+export default App;
