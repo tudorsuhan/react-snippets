758f487d919f23d54f87677e4baa38bbdf16591c
tudorsuhan <suhantudor@gmail.com>
2019-01-17 11:58:39 +0200

    init graphQL

--- graphql/src/App.js
+++ graphql/src/App.js
@@ -1,8 +1,16 @@
+// Core
 import React from 'react';
+
+// Instruments
 import './App.scss';
 
+// Components
+import LinkList from './components/LinkList';
+
 const App = () => (
-  <div className="App">App</div>
+  <div className="App">
+    <LinkList />
+  </div>
 );
 
 export default App;
--- graphql/src/index.js
+++ graphql/src/index.js
@@ -1,17 +1,36 @@
+// Core
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+// Instruments
 import configureStore from './store/configureStore';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:9000',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevtools: true,
+});
+
 render(
-  <Provider store={configureStore()}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={configureStore()}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root'),
 );
 
